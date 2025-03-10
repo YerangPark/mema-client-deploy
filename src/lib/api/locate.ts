94425a9f07241b89d804d2037909d7883d70bad9
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2025-03-10 11:44:00 +0900

    fix: api response변경

--- src/lib/api/locate.ts
+++ src/lib/api/locate.ts
@@ -29,10 +29,5 @@ export const createVoteLocation = async ({
   meetId: number;
   station: Station;
 }) => {
-  return await defaultAxios.post(`/meets/${meetId}/vote/location`, {
-    stationName: station.stationName,
-    routeName: station.lineName,
-    lat: station.lat,
-    lot: station.lot,
-  });
+  return await defaultAxios.post(`/meets/${meetId}/vote/location`, station);
 };
